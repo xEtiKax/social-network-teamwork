92ddb83f105879d68b72ad9ffb3161cae4fc63b2
Eti Ka <xetikax@gmail.com>
2020-04-14 21:32:41 +0300

    loged username homepage

--- src/main/java/com/example/demo/controllers/thymeleaf/HomeController.java
+++ src/main/java/com/example/demo/controllers/thymeleaf/HomeController.java
@@ -1,23 +1,35 @@
 package com.example.demo.controllers.thymeleaf;
 
+import com.example.demo.models.User;
 import com.example.demo.services.PostService;
+import com.example.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+
 @Controller
 public class HomeController {
     PostService postService;
+    UserService userService;
 
     @Autowired
-    public HomeController(PostService postService) {
+    public HomeController(PostService postService, UserService userService) {
         this.postService = postService;
+        this.userService = userService;
     }
 
     @GetMapping("/")
-    public String showHomePage(Model model) {
+    public String showHomePage(Model model, Principal principal) {
         model.addAttribute("posts", postService.getAllPublicPosts());
+        try {
+            User user = userService.getByUsername(principal.getName());
+            model.addAttribute("user", user);
+        }catch (Exception e){
+            System.out.println(e);
+        }
         return "index";
     }
 
