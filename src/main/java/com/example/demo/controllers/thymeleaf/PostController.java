498855f5e3fc40e9b036050c4bc841e34b7b8100
Eti Ka <xetikax@gmail.com>
2020-05-03 23:42:57 +0300

    post edit and delete in user-profile.html fixed

--- src/main/java/com/example/demo/controllers/thymeleaf/PostController.java
+++ src/main/java/com/example/demo/controllers/thymeleaf/PostController.java
@@ -3,6 +3,7 @@ package com.example.demo.controllers.thymeleaf;
 import com.example.demo.exceptions.AuthorizationException;
 import com.example.demo.exceptions.DuplicateEntityException;
 import com.example.demo.exceptions.EntityNotFoundException;
+import com.example.demo.models.DTO.CommentDTO;
 import com.example.demo.models.DTO.PostDTO;
 import com.example.demo.models.User;
 import com.example.demo.services.interfaces.PostService;
@@ -28,6 +29,7 @@ public class PostController {
         this.userService = userService;
     }
 
+    //index page
     @GetMapping("/new")
     public String showNewPostForm(Model model, Principal principal) {
         model.addAttribute("post", new PostDTO());
@@ -93,6 +95,7 @@ public class PostController {
         return "redirect:/";
     }
 
+    //myProfile
     @PostMapping("/user/add")
     public String addUserPost(@Valid @ModelAttribute("post") PostDTO postDTO, Model model, Principal principal) {
         try {
@@ -138,4 +141,47 @@ public class PostController {
         }
         return "redirect:/user/showMyProfile";
     }
+
+    // userProfile
+    @GetMapping("/userProfile/edit/{postId}/{userProfileId}")
+    public String showUserProfileEditPostForm(@PathVariable long postId, @PathVariable long userProfileId, Model model, Principal principal) {
+        User user = userService.getByUsername(principal.getName());
+        model.addAttribute("user", user);
+        model.addAttribute("userProfile", userService.getById(userProfileId));
+        model.addAttribute("friendsCounter", user.getFriends().size());
+        model.addAttribute("post", postService.getPostById(postId));
+        model.addAttribute("comment", new CommentDTO());
+        model.addAttribute("isFriend", true);
+        model.addAttribute("isFriendAndIsSentRequest", false);
+        model.addAttribute("isVisiblePicture", true);
+        model.addAttribute("isInfo", false);
+        return "user-profile";
+    }
+
+    @PostMapping("/userProfile/update/{postId}/{userProfileId}")
+    public String updateUserProfilePost(@PathVariable long postId, @PathVariable long userProfileId, @ModelAttribute PostDTO postDTO, Model model) {
+        try {
+            postService.updatePost(postId, postDTO);
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("error", e);
+            return "error";
+        }
+        return "redirect:/user/showUserProfile/" + userProfileId;
+    }
+
+    @PostMapping("/userProfile/delete/{postId}/{userProfileId}")
+    public String deleteUserProfilePost(@PathVariable long postId, @PathVariable long userProfileId, Model model, Principal principal, HttpServletRequest request) {
+        try {
+            User user = userService.getByUsername(principal.getName());
+            postService.deletePost(postId, user, request.isUserInRole("ROLE_ADMIN"));
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("error", e);
+            return "error";
+        } catch (AuthorizationException auth) {
+            model.addAttribute("auth", auth);
+        }
+        return "redirect:/user/showUserProfile/" + userProfileId;
+    }
+
+
 }
